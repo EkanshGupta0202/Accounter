cb8ed8e86a043f2f4b0f31ba6504a7adb40442cc
EkanshGupta0202 <61086429+EkanshGupta0202@users.noreply.github.com>
2020-06-09 20:01:46 +0530

    Update accounter.js

--- accounter.js
+++ accounter.js
@@ -1,18 +1,20 @@
 class Accounter{
     constructor(){
         this.input1 = createInput();
+        this.input2 = createInput();
         this.button1 = createButton('View balance');
         this.button2 = createButton('View transactions');
-        this.button3 = createButton('Hide balance');
+        this.button3 = createButton('Update Balance');
 
     }
 
 display(){
     this.input1.position(150,200);
+    this.input2.position(250,200);
+    this.input2.hide();
     this.button1.position(150,240);
     this.button2.position(350,50);
-    this.button3.position(150,240);
-    this.button3.hide();
+    this.button3.position(50,50);
     this.button1.mousePressed(()=>{
     amount = this.input1.value();
     balance = parseInt(balance)+parseInt(amount);
@@ -24,11 +26,12 @@ display(){
     //this.button1.hide();
     //this.button3.show();
     })
-    /*
+    
     this.button3.mousePressed(()=>{
-      t1.hide();
+      this.input2.show();
+      this.updateBalance(this.input2.value());
       })
-      */
+      
     this.button2.mousePressed(()=>{
     text("="+iterator,50,100);
     })

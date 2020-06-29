7f48c549bab33560027694efe1f3da02ce0e8174
EkanshGupta0202 <61086429+EkanshGupta0202@users.noreply.github.com>
2020-06-29 19:27:46 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,29 @@
+class Form{
+    constructor(){
+        this.input = createInput("",'password').attribute('placeholder', 'Enter Password');
+        this.button = createButton('Enter',);
+    }
+    display(){
+        this.input.position(150,200);
+        this.button.position(150,240);
+        this.button.mousePressed(()=>{
+        input = this.input.value();
+        })
+    }
+    getCount(){
+        var passwordRef = database.ref('Password');
+        passwordRef.on("value",(data)=>{
+          password = data.val();
+        })
+      }
+      hide(){
+          this.input.hide();
+          this.button.hide();
+      }
+      getHistory(){
+        var historyRef = database.ref('History');
+            historyRef.on("value",(data)=>{
+              iterator = data.val();
+            })
+          }
+}
